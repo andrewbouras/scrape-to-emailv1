d3dcb58c14d0709a41f61fe0324d05f16d8112ec
andrewbouras <94498047+andrewbouras@users.noreply.github.com>
2024-11-28 23:58:37 -0500

    Delete Scrape-to-email-v1 directory

--- Scrape-to-email-v1/scheduler.py
+++
@@ -1,9 +0,0 @@
-import schedule
-import time
-from generate import fetch_and_store_gi_studies
-
-schedule.every().day.at("00:00").do(fetch_and_store_gi_studies)
-
-while True:
-    schedule.run_pending()
-    time.sleep(60)
